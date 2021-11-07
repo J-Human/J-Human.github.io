6516f74a2d9c1427624e940290cf1b4cf0e14c24
J-Human <justinleeong@gmail.com>
2021-11-07 19:41:49 +0800

    Register Contacts file in router config

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
+import Contacts from './components/Contacts.vue';
 
 export default createRouter({
 	history: createWebHistory(),
-	routes: [{ path: '/', name: 'About me', component: App }],
+	routes: [
+		{ path: '/', name: 'About me', component: App },
+		{ path: '/contact', name: 'Contact', component: Contacts },
+	],
 });
