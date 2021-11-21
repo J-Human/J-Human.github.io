29da34528e0290aabb39eba07f03f91104103866
J-Human <justinleeong@gmail.com>
2021-11-21 20:33:44 +0800

    Actually implement contact icons

--- src/data/contacts.ts
+++ src/data/contacts.ts
@@ -1,42 +1,49 @@
 export default `
 	[github]
 	icon = "github"
+	brand = true
 	name = "GitHub"
 	value = "J-Human"
 	link = "https://github.com/J-Human"
 
 	[discord]
 	icon = "discord"
+	brand = true
 	name = "Discord"
 	value = "Justin ;)#2021"
 	link = "https://discord.com/users/746315246582366280"
 
 	[reddit]
 	icon = "reddit"
+	brand = true
 	name = "Reddit"
 	value = "Eternal_Dominus"
 	link = "https://www.reddit.com/user/Eternal_Dominus"
 
 	[keybase]
 	icon = "keybase"
+	brand = true
 	name = "Keybase"
 	value = "Justin101"
 	link = "https://keybase.io/Justin101"
 
 	[twitter]
 	icon = "twitter"
+	brand = true
 	name = "Twitter"
 	value = "justinleeong"
 	link = "https://twitter.com/justinleeong"
 
 	[email]
 	icon = "envelope"
+	brand = false
 	name = "Email"
 	value = "justinleeong@gmail.com"
 	link = "mailto:justinleeong@gmail.com"
 
 	[stackoverflow]
 	icon = "stack-overflow"
+	brand = true
 	name = "Stack Overflow"
 	value = "Justin"
 	link = "https://stackoverflow.com/users/16655757/justin"`;
--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -1,9 +1,13 @@
 <template>
 	<ul v-for="link in contacts" :key="link.name">
 		<li>
-			{{ link.name }}: <a :href="link.link" target="_blank" rel="noopener noreferrer">{{ link.value }}</a>
+			<i :class="`fa${link.brand ? '-brands' : 's'} fa-${link.icon}`" /> {{ link.name }}:
+			<a :href="link.link" target="_blank" rel="noopener noreferrer">{{ link.value }}</a>
 		</li>
 	</ul>
+	<footer>
+		<p>All logos are exclusive trademarks of their respective owners.</p>
+	</footer>
 </template>
 
 <script setup lang="ts">
@@ -21,9 +25,19 @@ a {
 a:visited {
 	color: #0000ff;
 }
+footer > p {
+	bottom: 1px;
+	font-size: 9px;
+	position: fixed;
+}
 ul {
 	font-size: 15px;
 	list-style: none;
 	text-align: center;
+}
+@media screen and (max-width: 320px) {
+	ul {
+		font-size: 10px;
+	}
 }
 </style>
