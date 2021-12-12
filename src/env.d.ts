222e45c244c85ef6860602e49f747c6f3ff3984f
J-Human <justinleeong@gmail.com>
2021-12-12 20:20:20 +0800

    Update dependencies, and general improvements

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,3 @@
-/* eslint-disable spaced-comment */
-/// <reference types="vite/client" />
-/// <reference types="vite-plugin-pages/client" />
-
 declare module '*.vue' {
 	import { DefineComponent } from 'vue';
 	const component: DefineComponent<Record<string, unknown>, Record<string, unknown>, unknown>;
